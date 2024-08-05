4d62bcb5fc8f5ab42bdc7650d55e4a1692ff6ad2
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-05 05:53:11 +0100

    Update dtypes.py

--- compressor/dtypes.py
+++ compressor/dtypes.py
@@ -2,6 +2,17 @@ from enum import Enum
 
 
 class CompressTypes(Enum):
+    """
+    An enumeration for data types used in compression and decompression.
+
+    Members:
+        INTEGER (1): Represents an integer data type.
+        STRING (2): Represents a string data type.
+        BOOLEAN (3): Represents a boolean data type.
+        LIST (4): Represents a list data type.
+        OBJECT (5): Represents an object (dictionary) data type.
+    """
+
     INTEGER = 1
     STRING = 2
     BOOLEAN = 3
