d3e9304916ed0eacf346cdcf6e4c1976e16432c6
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-05 05:54:02 +0100

    Update __init__.py

--- compressor/__init__.py
+++ compressor/__init__.py
@@ -1,3 +1,12 @@
 from .compressor import JsonCompressor
 from .decompressor import JsonDecompressor
 from .dtypes import CompressTypes
+
+
+__all__ = [
+    'JsonCompressor',
+    'JsonDecompressor',
+    'CompressTypes',
+]
+
+__author__ = "Hyper"
